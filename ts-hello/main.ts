b181d372284044751a07a1eeb83f3c5bc6a6b5ce
IsmailIbrahim84 <beisso75@hotmail.com>
2022-04-05 01:12:38 +0200

    Latest adding Interfaces and Classes

--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -20,4 +20,47 @@ const colorBlue=2;
 enum Color {Red, Green, Blue};
 let backgroundColor = Color.Red;
 
+//Using Type Assertion:
+let message;
+message = 'abc';
+let endsWithC = (<string>message).trim();
+let alternativeWay = (message as string).trim();
+
+//Arrow Function:
+let log = function (message) {
+    console.log(message);
+}
+
+let doLog = ()=> console.log();
+
+
+//Inline Annotation:
+//let drawPoint = (point: {x: number, y: number}) => {
+    //..
+//}
+
+interface Point {x: number, y: number, draw: () => void}
+
+let drawPoint = (point: Point)=> {
+
+}
+
+drawPoint({
+    draw(): void {
+    }, x:1 , y:2})
+
+
+//Adding class:
+class PointClass {
+    x: number;
+    y: number;
+
+    draw(){
+
+    }
+
+    getDistance(another: Point){
+        
+    }
+}
 
