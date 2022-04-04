ffc6ff6ff76da2e269f3a1d53b8f72d1e0b09647
IsmailIbrahim84 <beisso75@hotmail.com>
2022-04-05 00:52:31 +0200

    Latest

--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,6 +1,23 @@
-function log(message)
-{
-    console.log(message);
-}
-var message = 'Hello World';
-log(message);
+// function log(message)
+// {
+//     console.log(message);
+// }
+// var message = 'Hello World';
+// log(message);
+
+
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1,2,3];
+let f: any[] =  [1, true, 'a', false];
+
+const colorRed=0;
+const colorGreen=1;
+const colorBlue=2;
+
+enum Color {Red, Green, Blue};
+let backgroundColor = Color.Red;
+
+
